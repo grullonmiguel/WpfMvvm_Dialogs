4b23fc6cf5a1e7eabcfc918f1fef4ae931408787
grullonmiguel <grullonmiguel@gmail.com>
2020-02-04 12:09:56 -0500

    Adds WPF UI Project
    Added empty WPF UI project (.NET Core)

---
+++ DesktopUI/Views/ShellView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WPF.UI.Views
+{
+    public partial class ShellView : Window
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
