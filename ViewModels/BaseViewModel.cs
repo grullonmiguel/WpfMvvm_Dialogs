85a1a02050a5bb495f3501a90c3273f00c64549d
grullonmiguel <grullonmiguel@gmail.com>
2020-02-04 11:57:58 -0500

    Adds ViewModel Library
    Added empty ViewModel class library to the project

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
